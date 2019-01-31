204afb4afeaef2fb2b530bb5e08ca3f31ba1dda4
MUSTOFA SACHI <mustofa_sachi@hotmail.com>
2019-01-31 15:49:38 +0700

    add Palindromw

--- PalindromSen.js
+++ PalindromSen.js
@@ -3,7 +3,6 @@ var str = "ppPp"
 console.log("String : "+str)
 var status = false
 let mid = (str.length /2) - 1;
-str = str.toLowerCase();
 for(var i=mid+1 ,j= mid;i<str.length-1,j>=0;i++,j--){
     if(mid==isNaN()){
         if(str[i]==str[j]){
