b481eeb5302bf89361934b8ae6ea725692cadf16
boatdk <boatz.dk@gmail.com>
2019-03-21 17:20:07 +0700

    add redux

--- lab 4/lab4/src/App.js
+++ lab 4/lab4/src/App.js
@@ -3,24 +3,63 @@ import './App.css';
 // import TaskList from './todo/TaskList'
 // import InputTask from "./todo/InputTask";
 import Github from "./github";
-import Content from "./Content"
-import Header from "./Header"
-import Data from './Data'
+// import Content from "./Content"
+// import Header from "./Header"
+// import Data from './Data'
+import {Provider} from 'react-redux'
+import {logger} from 'redux-logger'
+import thunk from 'redux-thunk'
+import axios from '../node_modules/axios';
+import { combineReducers, createStore, applyMiddleware } from '../node_modules/redux';
+
+export const getApiSuccess = data => ({ type: 'GET_API_SUCCESS', data})
+export const getApiFailed = () => ({type:'GET_API_FAILED'})
+export const getApi = (USER) => async (dispatch) => {
+    try{
+        console.log('get data')
+        const res = await axios.get(`https://api.github.com/users/${USER}`)
+        console.log(res)
+        const resBody = await res.data
+        console.log(res.data)
+        console.log('res:' , resBody)
+        dispatch(getApiSuccess(resBody))
+    } catch(error){
+        console.error(error)
+        dispatch(getApiFailed())
+    }
+    console.log(store)
+}
+
+export const githubReducer = (state = 0, action) => {
+    switch(action.type){
+        case "GET_API_SUCCESS":
+            return action.data
+        case 'GET_API_FAILED':
+            return state
+        default:
+            return state
+    }
+}
+
+export const rootReducer = combineReducers({data: githubReducer})
+
+export const store = createStore(rootReducer, applyMiddleware(thunk,logger))
+
 
 class App extends Component {
 
 
-   state = {
-       tasks: [{id: 1, task: 'Do homework', place: 'Library'},
-               {id: 2, task: 'Read book', place: 'Cafe'}],
-       id:3
-   }
+//    state = {
+//        tasks: [{id: 1, task: 'Do homework', place: 'Library'},
+//                {id: 2, task: 'Read book', place: 'Cafe'}],
+//        id:3
+//    }
 
-   addTask = (task,place) => {
-       this.setState({
-                tasks: [...this.state.tasks, {id: this.state.id, task, place } ],
-                id: this.state.id+1  })
-   }
+//    addTask = (task,place) => {
+//        this.setState({
+//                 tasks: [...this.state.tasks, {id: this.state.id, task, place } ],
+//                 id: this.state.id+1  })
+//    }
 
   //  deleteTask = (id) => {
 
@@ -30,16 +69,10 @@ class App extends Component {
 
 
        return (
-           <div className="App">
-           <div class="space">
-           <Header/>
-           </div>
-           
-           <Content/>
-           <Data/>
-            
-           </div>
-       );
+           <Provider store={store}>
+            <Github/>
+           </Provider>
+       )
    }
 }
 
--- lab 4/lab4/src/github/index.js
+++ lab 4/lab4/src/github/index.js
@@ -1,27 +1,56 @@
 import React, {Component}  from 'react';
-import axios from "axios"
+// import axios from "axios"
+import {getApi} from '../App'
+import {connect} from 'react-redux'
 
 class Github extends Component {
 
-    state = { user: 'wwarodom', data:''}
- 
-    componentDidMount = () => this.fetchUser(this.state.user)
- 
-    fetchUser = (USER) => {
-        axios.get(`https://api.github.com/users/${USER}`)
-            .then(res=> {
-                this.setState({data: res.data})
-                console.log(res.data)
-            })
+    componentDidMount(){
+        console.log('props', this.props)
+        this.props.getApi()
     }
+
+    // state = { user: 'wwarodom', data:''}
  
+    // componentDidMount = () => this.fetchUser(this.state.user)
+ 
+    // fetchUser = (USER) => {
+    //     axios.get(`https://api.github.com/users/${USER}`)
+    //         .then(res=> {
+    //             this.setState({data: res.data})
+    //             console.log(res.data)
+    //         })
+    // }
+
+    
+    renderUser = () => {
+        // console.log(data)
+        console.log('props: ', this.props.data)
+        if(this.props.data){
+           return this.props.data.url
+           
+        }
+    }
     render() {
-        const {data} = this.state
-        if (data)
-            return ( <div>
-                {data.id}: {data.name} <img src={data.avatar_url} alt="avatar" width="50px"/> </div> )
-        return (<div>.</div>);
+      return (
+          <div>
+              <h2> RENDER USER</h2>  
+              <ul>
+                  {this.renderUser()}
+              </ul>
+          </div>
+      )
     }
  }
+
+ const mapStateToProps = ({data}) => {
+     return {data}
+ }
+
+ const mapDispatchToProps = (dispatch) => {
+     return {
+         getApi: () => dispatch(getApi())
+     }
+ }
  
- export default Github
+ export default connect (mapStateToProps,mapDispatchToProps)(Github)
